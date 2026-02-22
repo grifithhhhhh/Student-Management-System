2ee550fdbd8d601a2533370dfd760b573b38b2f3
grifithhhhhh <pushpakbu@gmail.com>
2026-02-22 17:39:38 +0530

    connected MongoDB

--- backend/index.js
+++ backend/index.js
@@ -2,30 +2,30 @@ const express = require("express")
 const PORT = 8004
 const app = express()
 const fs = require("fs")
-app.use(express.json());
 const {createLogs} = require("./middlewares/log")
-
+const  mongoose = require("mongoose")
 
 // Middleware--------------------------------------
-app.use(createLogs("log.txt"));
+    app.use(createLogs("log.txt"));
+    app.use(express.json());
 
-
+// CONNECTION ------------------------
+    mongoose.connect('mongodb://127.0.0.1:27017/studentserver')
+    .then(() => {console.log("MongoDB connected")})
+    .catch(err => console.log(err))
+    
 // ROUTES -----------------------------
-app.get('/', (req,res)=> {
-    res.send("the server is running")
-})
+    app.get('/', (req,res)=> {
+        res.send("the server is running")
+    })
 
-app.post('/users', (req,res)=> {
-    const body = req.body;
-    console.log(body);
-
-    res.json(
-        {messageStatus : "recieved",
-            data: body
-        }
-    )
-})
+    app.post('/', (req,res)=> {
+        const Body = req.body;
+        console.log(Body);
+        res.json(Body)
+    })
 
 // SERVER -------------------------------------------------------
-app.listen(PORT, ()=> {console.log("Server started @ : ", PORT)})
-console.log("welcome to student management system")
+    app.listen(PORT, ()=> {console.log("Server started @ : ", PORT)})
+    console.log("welcome to student management system")
+
